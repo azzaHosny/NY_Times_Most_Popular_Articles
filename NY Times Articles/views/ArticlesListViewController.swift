a93b808f2ac21bd0ad9a4c9bec91a7f30bb845b0
Azah mahgoub <azza.mahgoub@du.ae>
2023-05-13 21:01:20 +0400

    intiate app base

---
+++ NY Times Articles/views/ArticlesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ArticlesListViewController.swift
+//  NY Times Articles
+//
+//  Created by Azah mahgoub on 13/05/2023.
+//
+
+import UIKit
+
+class ArticlesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
